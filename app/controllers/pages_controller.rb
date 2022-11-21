3cadad46e8a4987610c161c64dd39ac0285cc80b
Eduardo Oliveira <edu.oliveeira@hotmail.com>
2022-11-21 18:02:39 -0300

    Home e Navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,21 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_super_power, only: %i[show]
 
   def home
-    @super_power = SuperPower.all
+    @super_powers = SuperPower.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_super_power
+    @super_power = SuperPower.find(params[:id])
+  end
+
+  def super_power_params
+    params.require(:super_power).permit(:name, :description, :price)
   end
 end
