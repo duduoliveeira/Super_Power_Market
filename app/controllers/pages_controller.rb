fa1e61adfa66adf8a46eb8c45232cb0b23026c37
tomasherdy <tomasherdy@hotmail.com>
2022-11-21 18:03:56 -0300

    show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_super_power, only: :show
+
 
   def home
     @super_power = SuperPower.all
+  end
+
+  def show
+    @users = User.all
+  end
+
+  private
+
+  def set_super_power
+    @super_power = SuperPower.find(params[:id])
   end
 end
