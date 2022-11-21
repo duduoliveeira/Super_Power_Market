93f02e8a7a8139c1d507f69f7814443282509f67
Eduardo Oliveira <edu.oliveeira@hotmail.com>
2022-11-21 15:09:51 -0300

    Seed create

---
+++ app/controllers/super_powers_controller.rb
@@ -0,0 +1,2 @@
+class SuperPowersController < ApplicationController
+end
---
+++ test/models/super_power_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SuperPowerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
