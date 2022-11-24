3b24c35540d94ac47bc6e458c403fe6d44276f23
Eduardo Oliveira <edu.oliveeira@hotmail.com>
2022-11-24 11:00:42 -0300

    Search implementada

--- app/models/super_power.rb
+++ app/models/super_power.rb
@@ -1,4 +1,12 @@
 class SuperPower < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+                  against: %i[name description],
+                  ignoring: :accents,
+                  associated_against: {
+                    user: [:name]
+                  },
+                  using: { tsearch: { prefix: true } }
   belongs_to :user
   validates :name, :description, :price, presence: true
 end
